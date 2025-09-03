047833ba00dbd8b0731a6dceb76c8067c5bc2040
caiocerqueirajs <caiocerqueira.silva@ucsal.edu.br>
2025-09-02 23:10:33 -0300

    Alterei Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
 import br.com.mariojp.solid.ocp.*;
 
 public class Main {
-	public static void main(String[] args) {
-		DiscountCalculator calc = new DiscountCalculator();
-		System.out.println("REGULAR 100 -> " + calc.apply(100, CustomerType.REGULAR));
-		System.out.println("PREMIUM 100 -> " + calc.apply(100, CustomerType.PREMIUM));
-		// PARTNER deveria ter 12%
-		System.out.println("PARTNER 100 -> " + calc.apply(100, CustomerType.PARTNER));
-	}
+    public static void main(String[] args) {
+        Map<CustomerType, DiscountPolicy> policies = new HashMap<>();
+        policies.put(CustomerType.REGULAR, new RegularPolicy());
+        policies.put(CustomerType.PREMIUM, new PremiumPolicy());
+        policies.put(CustomerType.PARTNER, new PartnerPolicy());
+
+        DiscountCalculator calc = new DiscountCalculator(policies);
+
+        System.out.println("REGULAR 100 -> " + calc.apply(100, CustomerType.REGULAR));
+        System.out.println("PREMIUM 100 -> " + calc.apply(100, CustomerType.PREMIUM));
+        System.out.println("PARTNER 100 -> " + calc.apply(100, CustomerType.PARTNER));
+    }
 }
